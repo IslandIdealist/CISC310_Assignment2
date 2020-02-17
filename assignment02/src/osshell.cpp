3f4abec68c9cdbc3f7d682d83d05aa9c5921bf2b
Yuzu17 <46982055+Yuzu17@users.noreply.github.com>
2020-02-17 17:11:00 -0600

    Add files via upload

--- assignment02/src/osshell.cpp
+++ assignment02/src/osshell.cpp
@@ -3,7 +3,11 @@
 #include <string>
 #include <vector>
 #include <sstream>
- 
+#include <dirent.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fstream>
+
 std::vector<std::string> splitString(std::string text, char d);
 std::string getFullPath(std::string cmd, const std::vector<std::string>& os_path_list);
 bool fileExists(std::string full_path, bool *executable);
@@ -15,6 +19,19 @@ int main (int argc, char **argv)
     std::vector<std::string> os_path_list = splitString(os_path, ':');
 
     std::cout << "Welcome to OSShell! Please enter your commands ('exit' to quit)." << std::endl;
+
+std::string fullpath = getFullPath("ls", os_path_list);
+bool *executable;
+fileExists(fullpath, executable);
+
+
+     /*pid_t pid;
+     char *const arr[] = {"ls", "-l", "-R","-a", NULL};
+	execv("ls", arr);*/
+
+	/*char *bin_path = "ls";
+	char *args[]={bin_path,"-a", "-s",NULL};
+        execv(bin_path,args);*/
 
     // Repeat:
     //  Print prompt for user input: "osshell> " (no newline)
@@ -55,8 +72,30 @@ std::vector<std::string> splitString(std::string text, char d)
 // Returns a string for the full path of a command if it is found in PATH, otherwise simply return ""
 std::string getFullPath(std::string cmd, const std::vector<std::string>& os_path_list)
 {
+
+	//std::string last_element(cmd.substr(cmd.rfind("/") + 1));
+	//std::cout << last_element << "   HERE " << std::endl;
+    DIR *dp;
+    struct dirent *dirp;
+
+
+      //files.push_back(string(dirp->d_name));
+    //closedir(dp);
+
 	for(int i=0; i<os_path_list.size(); i++){
-		//if( cmd.compare(os_path_list)==0 ){ }
+		
+	    	if((dp = opendir(os_path_list[i].c_str())) == NULL)
+    		{
+      		std::cout << "Error(" << errno << ") opening " << cmd << std::endl;
+      		//return errno;
+    		}
+    		while ((dirp = readdir(dp)) != NULL){
+			std::string fname = dirp->d_name;
+			if( fname.find(cmd) !=  std::string::npos){
+				std::cout << os_path_list[i] << "/"<<fname << "/"<< cmd << std::endl;
+				return os_path_list[i] + "/" + fname + "/" + cmd;
+			}
+    		}
 	}
     return "";
 }
@@ -66,5 +105,12 @@ std::string getFullPath(std::string cmd, const std::vector<std::string>& os_path
 bool fileExists(std::string full_path, bool *executable)
 {
     *executable = false;
-    return false;
+    std::ifstream f(full_path);
+	std::cout << full_path << std::endl;
+	if(f.good()){
+		*executable = true;
+		return true;
+	}else{
+		return false;
+	}
 }
